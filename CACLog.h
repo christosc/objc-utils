dfb2ebc5c79061011d0d6355aaea06cf8633a47a
Christos Chryssochoidis <c.chryssochoidis@gmail.com>
2014-05-24 21:56:11 +0300

    Rename logging function.

--- CACLog.h
+++ CACLog.h
@@ -1,3 +1,3 @@
-#define CHCLog(format,...) NSLog(@"[%@ %@ %p]" format,\
+#define CACLog(format,...) NSLog(@"[%@ %@ %p]" format,\
 	NSStringFromClass([self class]),\
 	NSStringFromSelector(_cmd), self, ##__VA_ARGS__);
